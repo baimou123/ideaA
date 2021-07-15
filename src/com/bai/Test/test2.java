57d0cdb86fc479a80af9635ff8df6c8ac709180e
bai_mou <baimou0103@163.com>
2021-07-15 08:17:11 +0800

    7.14作业

--- src/com/bai/Test/test2.java
+++ src/com/bai/Test/test2.java
@@ -7,7 +7,7 @@ public class test2 {
     public static void main(String[] args) {
         Date date=new Date();
         String partten="yyyy-MM-dd HH:mm:ss";
-        SimpleDateFormat sdf = new SimpleDateFormat();
+        SimpleDateFormat sdf = new SimpleDateFormat(partten);
         String a = sdf.format(date);
         System.out.println("a = " + a);
         
